7feb7fa77a347fc713382129ce21bc932b346c2a
Markus Wyss <wyssmarkus@msn.com>
2022-04-23 13:41:14 +0200

    Add WallCorner
    Corner for smooth walls

--- Assets/Scripts/MapTools/MapGenerator.cs
+++ Assets/Scripts/MapTools/MapGenerator.cs
@@ -2,19 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
 {
-    private Dictionary<int, GameObject> tileSet;
-    private Dictionary<int, GameObject> tileGroups;
-    private (int x , int y) mapSize = (50, 50);
-    private List<List<int>> noiseGrid = new List<List<int>>();
-    private List<List<GameObject>> tileGrid = new List<List<GameObject>>();
+    private Dictionary<int, GameObject> _tileSet;
+    private Dictionary<int, GameObject> _tileGroups;
+    private (int x , int y) _mapSize = (50, 50);
+    private readonly List<List<GameObject>> _tileGrid = new List<List<GameObject>>();
+    private List<List<bool>> _mapMatrix;
+    private int _randomFillPercent = 35;
+    
+    //Prefabs
     public GameObject prefabWallBlack;
+    public GameObject prefabWallBlackCorner;
+    public GameObject prefabWallBlackDtr; 
     public GameObject prefabFloor;
 
-    private int randomFillPercent = 35;
+    
     
     void Start()
     {
@@ -36,58 +42,57 @@ public class MapGenerator : MonoBehaviour
 
     private void ClearMap()
     {
-        foreach (var mapY in tileGrid)
+        foreach (var mapY in _tileGrid)
         {
             foreach (var tile in mapY)
             {
-                GameObject.Destroy(tile.gameObject);
+                Destroy(tile.gameObject);
             }
         }
-        noiseGrid.Clear();
-        tileGrid.Clear();
+        _tileGrid.Clear();
         
     }
 
     private void SetMapValue()
     {
-        randomFillPercent = (int) (Random.value * 10 + 35);
-        mapSize.x = (int) (Random.value * 30 + 40);
-        mapSize.y = (int) (Random.value * 30 + 40);
+        _randomFillPercent = (int) (Random.value * 10 + 35);
+        _mapSize.x = (int) (Random.value * 30 + 40);
+        _mapSize.y = (int) (Random.value * 30 + 40);
     }
     
     private void CreateTileSet()
     {
-        tileSet = new Dictionary<int, GameObject>()
+        _tileSet = new Dictionary<int, GameObject>()
         {
             {0, prefabWallBlack},
-            {1, prefabFloor}
+            {1, prefabWallBlackCorner},
+            {2, prefabFloor},
+            {3, prefabWallBlackDtr}
         };
     }
 
     private void CreateTileGroups()
     {
-        tileGroups = new Dictionary<int, GameObject>();
-        foreach (var keyValuePair in tileSet)
+        _tileGroups = new Dictionary<int, GameObject>();
+        foreach (var keyValuePair in _tileSet)
         {
             GameObject tileGroup = new GameObject(keyValuePair.Value.name);
             tileGroup.transform.parent = gameObject.transform;
             tileGroup.transform.localPosition = new Vector3(0, 0, 0);
-            tileGroups.Add(keyValuePair.Key, tileGroup);
+            _tileGroups.Add(keyValuePair.Key, tileGroup);
         }
     }
 
     private void GenerateMap()
     {
-        List<List<bool>> mapMatrix = new MapMatrixGenerator().GetMapMatrix((mapSize.x, mapSize.y), randomFillPercent);
+        _mapMatrix = new MapMatrixGenerator().GetMapMatrix((_mapSize.x, _mapSize.y), _randomFillPercent);
         
-        for (int x = 0; x < mapSize.x; x++)
+        for (int x = 0; x < _mapSize.x; x++)
         {
-            noiseGrid.Add(new List<int>());
-            tileGrid.Add(new List<GameObject>());
-            for (int y = 0; y < mapSize.y; y++)
+            _tileGrid.Add(new List<GameObject>());
+            for (int y = 0; y < _mapSize.y; y++)
             {
-                int tileId = mapMatrix[x][y]? 0: 1;
-                noiseGrid[x].Add(tileId);
+                int tileId = _mapMatrix[x][y]? 0: 2;
                 CreateTile(tileId, (x, y));
             }
         }
@@ -95,19 +100,185 @@ public class MapGenerator : MonoBehaviour
 
     private void CreateTile(int tileId, (int x, int y) coordinate)
     {
-        GameObject tilePrefab = tileSet[tileId];
-        GameObject tileGroup = tileGroups[tileId];
+        var wallVersion = WallVersions.Square;
+        if (tileId == 0)
+        {
+            wallVersion = GetWallVersion(coordinate);
+        }
+
+        if (wallVersion == WallVersions.CornerBL||
+            wallVersion == WallVersions.CornerBR||
+            wallVersion == WallVersions.CornerTL||
+            wallVersion == WallVersions.CornerTR)
+        {
+            tileId = 1;
+        }
+        
+        if (wallVersion == WallVersions.DiagonalBL||
+            wallVersion == WallVersions.DiagonalBR||
+            wallVersion == WallVersions.DiagonalTL||
+            wallVersion == WallVersions.DiagonalTR)
+        {
+            tileId = 3;
+        }
+        
+        GameObject tilePrefab = _tileSet[tileId];
+        GameObject tileGroup = _tileGroups[tileId];
+        
+        if (tileId == 1)
+        {
+            switch (wallVersion)
+            {
+                case WallVersions.CornerBL:
+                    Rotate(180);
+                    break;
+                case WallVersions.CornerBR:
+                    Rotate(270);
+                    break;
+                case WallVersions.CornerTL:
+                    Rotate(90);
+                    break;
+            }
+        }
+        
+        if (tileId == 3)
+        {
+            switch (wallVersion)
+            {
+                case WallVersions.DiagonalBL:
+                    Rotate(180);
+                    break;
+                case WallVersions.DiagonalBR:
+                    Rotate(270);
+                    break;
+                case WallVersions.DiagonalTL:
+                    Rotate(90);
+                    break;
+            }
+        }
+        
         GameObject tile = Instantiate(tilePrefab, tileGroup.transform);
 
         tile.name = string.Format("tileX{0}Y{1}", coordinate.x, coordinate.y);
         tile.transform.localPosition = new Vector3(coordinate.x, coordinate.y, 0);
         
-        tileGrid[coordinate.x].Add(tile);
+        _tileGrid[coordinate.x].Add(tile);
+        
+        tilePrefab.transform.eulerAngles = new Vector3(0,0,0);
+        
+        void Rotate(int degree) => tilePrefab.transform.eulerAngles = Vector3.forward * degree;
     }
 
     private void SetMapObjects()
     {
         var player = GameObject.Find("Player");
-        player.transform.localPosition = new Vector3(mapSize.x / 2, mapSize.x / 2, 0);
+        player.transform.localPosition = new Vector3(_mapSize.x / 2, _mapSize.x / 2, 0);
+    }
+
+    public WallVersions GetWallVersion((int x, int y) coordinate)
+    {
+        if ( _mapMatrix is null)
+        {
+            return WallVersions.Square;
+        }
+
+        if (coordinate.x == 0 ||
+            coordinate.x == _mapMatrix.Count - 1 ||
+            coordinate.y == 0 ||
+            coordinate.y == _mapMatrix[coordinate.x].Count - 1)
+        {
+            return WallVersions.Square;
+        }
+
+        if (_mapMatrix[coordinate.x - 1][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y - 1] == false &&
+            _mapMatrix[coordinate.x - 1][coordinate.y] == true &&
+            _mapMatrix[coordinate.x][coordinate.y - 1] == true
+            )
+        {
+            return WallVersions.CornerTR;
+        }
+        
+        if (_mapMatrix[coordinate.x - 1][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x - 1][coordinate.y] == false &&
+            _mapMatrix[coordinate.x - 1][coordinate.y - 1] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y] == true &&
+            _mapMatrix[coordinate.x][coordinate.y - 1] == true
+           )
+        {
+            return WallVersions.CornerTL;
+        }
+        
+        if (_mapMatrix[coordinate.x - 1][coordinate.y - 1] == false &&
+            _mapMatrix[coordinate.x][coordinate.y - 1] == false &&
+            _mapMatrix[coordinate.x - 1][coordinate.y - 1] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x ][coordinate.y + 1] == true &&
+            _mapMatrix[coordinate.x][coordinate.y - 1] == true
+           )
+        {
+            return WallVersions.CornerBR;
+        }
+        
+        if (_mapMatrix[coordinate.x - 1][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x][coordinate.y - 1] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y - 1] == false &&
+            _mapMatrix[coordinate.x - 1][coordinate.y] == false &&
+            _mapMatrix[coordinate.x - 1][coordinate.y + 1] == false &&
+            _mapMatrix[coordinate.x + 1][coordinate.y] == true &&
+            _mapMatrix[coordinate.x][coordinate.y + 1] == true
+           )
+        {
+            return WallVersions.CornerBL;
+        }
+        
+        return WallVersions.Square;
+        
+        
+        //  1 # 2 # 3
+        //  #########
+        //  4 # 5 # 7
+        //  #########
+        //  6 # 8 # 9
+        (int x, int y) GetPrefix(int a) => a switch
+        {
+            1 => (-1, 1),
+            2 => (0, 1),
+            3 => (1, 1),
+            4 => (-1, 0),
+            5 => (0, 0),
+            6 => (1, 0),
+            7 => (-1, -1),
+            8 => (0, -1),
+            9 => (1, -1),
+            _ => (0, 0)
+        };
+        
+    }
+    
+    public enum TileTypes
+    {
+        WallSquare,
+        WallCorner,
+        Floor
+    }
+    
+    public enum WallVersions
+    {
+        Square,
+        CornerTL,
+        CornerTR,
+        CornerBL,
+        CornerBR,
+        DiagonalTL,
+        DiagonalTR,
+        DiagonalBL,
+        DiagonalBR
     }
 }
