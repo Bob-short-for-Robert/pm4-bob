47ab89091f89a6c735b5ce0cc73b85cc63fc7d2b
DonTars <fischer.gabriel@outlook.de>
2022-05-03 23:44:48 +0200

    further menus and tests

--- Assets/AltUnityTester/Editor/Tests/MenuTest.cs
+++ Assets/AltUnityTester/Editor/Tests/MenuTest.cs
@@ -5,13 +5,12 @@ namespace AltUnityTester.Editor.Tests
 {
     public class MenuTest
     {
-        public Altom.AltUnityDriver.AltUnityDriver altUnityDriver;
+        public AltUnityDriver altUnityDriver;
         //Before any test it connects with the socket
         [OneTimeSetUp]
         public void SetUp()
         {
-            altUnityDriver = new Altom.AltUnityDriver.AltUnityDriver();
-            altUnityDriver.LoadScene("Menu");
+            altUnityDriver = new AltUnityDriver();
         }
 
         //At the end of the test closes the connection with the socket
@@ -22,14 +21,47 @@ namespace AltUnityTester.Editor.Tests
         }
         
         [Test]
-        public void TestOptions()
+        public void TestSettings()
         {
+            altUnityDriver.LoadScene("Menu");
             // check initial state
             Assert.True(altUnityDriver.FindObject(By.NAME, "MainMenu").enabled);
             
             //switch to options menu and check if it is set as active
-            altUnityDriver.Click(altUnityDriver.FindObject(By.NAME, "OptionsButton").getScreenPosition());
-            Assert.True(altUnityDriver.FindObject(By.NAME, "OptionsMenu").enabled);
+            altUnityDriver.Click(altUnityDriver.FindObject(By.NAME, "SettingsButton").getScreenPosition());
+            Assert.True(altUnityDriver.FindObject(By.NAME, "SettingsMenu").enabled);
+            
+            // return to main menu
+            altUnityDriver.Click(altUnityDriver.FindObject(By.NAME, "BackButton").getScreenPosition());
+            Assert.True(altUnityDriver.FindObject(By.NAME, "MainMenu").enabled);
+        }
+
+        [Test]
+        public void TestStatistics()
+        {
+            altUnityDriver.LoadScene("Menu");
+            // check initial state
+            Assert.True(altUnityDriver.FindObject(By.NAME, "MainMenu").enabled);
+            
+            //switch to options menu and check if it is set as active
+            altUnityDriver.Click(altUnityDriver.FindObject(By.NAME, "StatisticsButton").getScreenPosition());
+            Assert.True(altUnityDriver.FindObject(By.NAME, "StatisticsMenu").enabled);
+            
+            // return to main menu
+            altUnityDriver.Click(altUnityDriver.FindObject(By.NAME, "BackButton").getScreenPosition());
+            Assert.True(altUnityDriver.FindObject(By.NAME, "MainMenu").enabled);
+        }
+
+        [Test]
+        public void TestControls()
+        {
+            altUnityDriver.LoadScene("Menu");
+            // check initial state
+            Assert.True(altUnityDriver.FindObject(By.NAME, "MainMenu").enabled);
+            
+            //switch to options menu and check if it is set as active
+            altUnityDriver.Click(altUnityDriver.FindObject(By.NAME, "ControlsButton").getScreenPosition());
+            Assert.True(altUnityDriver.FindObject(By.NAME, "ControlsMenu").enabled);
             
             // return to main menu
             altUnityDriver.Click(altUnityDriver.FindObject(By.NAME, "BackButton").getScreenPosition());
@@ -39,6 +71,7 @@ namespace AltUnityTester.Editor.Tests
         [Test]
         public void TestStart()
         {
+            altUnityDriver.LoadScene("Menu");
             // check initial state
             Assert.True(altUnityDriver.FindObject(By.NAME, "MainMenu").enabled);
             
