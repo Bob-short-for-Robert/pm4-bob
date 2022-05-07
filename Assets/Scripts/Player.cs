dcd268ff08f0043f49fd74ef1e9e2ee9663bf3ee
Markus Wyss <wyssmarkus@msn.com>
2022-05-07 16:58:31 +0200

    rework Player for better Weapon behavior

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Runtime.CompilerServices;
 using static System.Console;
 
 [RequireComponent(typeof(BoxCollider2D))]
@@ -10,14 +11,15 @@ public class Player : MonoBehaviour
 {
     public float playerSpeed = 2.0f;  
     
-    private BoxCollider2D boxCollider;
-    private RaycastHit2D raycasthit;
-    private Vector3 moveDelta;
+    private BoxCollider2D _boxCollider;
+    private RaycastHit2D _raycastHit;
+    private Vector3 _moveDelta;
+    private bool _facingLeft;
 
 
     private void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
@@ -26,27 +28,42 @@ public class Player : MonoBehaviour
         float y = Input.GetAxis("Vertical");
 
         // reset moveDelta 
-        moveDelta = new Vector3(x, y, 0);
+        _moveDelta = new Vector3(x, y, 0);
 
         // swap Sprite direction
-        if (moveDelta.x < 0)
-            transform.localScale = Vector3.one;
-        else if (moveDelta.x > 0)
-            transform.localScale = new Vector3(-1, 1, 1);
+        if (_moveDelta.x < 0 && _facingLeft)
+        {
+            FlipPlayer();
+        }
+            
+        else if (_moveDelta.x > 0 && !_facingLeft)
+        {
+            FlipPlayer();
 
+        }
+            
         var collisionMasks = LayerMask.GetMask("Actor", "Blocking");
-        raycasthit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y),
-            Math.Abs(moveDelta.y * Time.deltaTime), collisionMasks);
-        if (raycasthit.collider == null)
+        _raycastHit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(0, _moveDelta.y),
+            Math.Abs(_moveDelta.y * Time.deltaTime), collisionMasks);
+        if (_raycastHit.collider == null)
         {
-            transform.Translate(0, moveDelta.y * Time.deltaTime * playerSpeed, 0);
+            transform.Translate(0, _moveDelta.y * Time.deltaTime * playerSpeed, 0);
         }
 
-        raycasthit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0),
-            Math.Abs(moveDelta.x * Time.deltaTime), collisionMasks);
-        if (raycasthit.collider == null)
+        _raycastHit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(_moveDelta.x, 0),
+            Math.Abs(_moveDelta.x * Time.deltaTime), collisionMasks);
+        if (_raycastHit.collider == null)
         {
-            transform.Translate(moveDelta.x * Time.deltaTime *playerSpeed, 0, 0);
+            transform.Translate(
+                _moveDelta.x * Time.deltaTime *playerSpeed * (_facingLeft ? -1 : 1),
+                0,
+                0);
         }
+    }
+
+    private void FlipPlayer()
+    {
+        _facingLeft = !_facingLeft;
+        transform.Rotate(0f,180f, 0f);
     }
 }
