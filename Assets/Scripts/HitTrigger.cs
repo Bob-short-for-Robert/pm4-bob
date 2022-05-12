51f81dda127f50f946e2845e18c06802ec58960f
nitscman <nitscman@students.zhaw.ch>
2022-05-12 09:43:30 +0200

    spill blood of player when touched and add iframes after being hit

--- Assets/Scripts/HitTrigger.cs
+++ Assets/Scripts/HitTrigger.cs
@@ -1,19 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MapTools;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class HitTrigger : MonoBehaviour
 {
     public GameObject[] Effects;
+    public float iframes = 0.5f;
+    private float _invincibilityTimer = 0f;
+
+    private void Update()
+    {
+        _invincibilityTimer -= Time.deltaTime;
+    }
 
     private void OnTriggerEnter2D(Collider2D colliderData)
     {
-        if (colliderData.tag == "Projectile")
+        if (_invincibilityTimer > 0) return;
+        _invincibilityTimer = iframes;
+        var map = GameObject.Find("Map");
+        var mapController = (MapGenerator) map.GetComponent(typeof(MapGenerator));
+
+        if (colliderData.CompareTag("Projectile") && gameObject.CompareTag("Enemy")) // enemy hit projectile
         {
-            var map = GameObject.Find("Map");
-            var mapController = (MapGenerator) map.GetComponent(typeof(MapGenerator));
-            mapController.AddGameObject(Effects[Random.Range(0, Effects.Length)], transform.position, Random.Range(0, 360));
+            mapController.AddGameObject(Effects[Random.Range(0, Effects.Length)], transform.position,
+                Random.Range(0, 360));
+        }
+
+        if (colliderData.CompareTag("Player") && gameObject.CompareTag("Enemy")) //enemy hit player)
+        {
+            mapController.AddGameObject(Effects[Random.Range(0, Effects.Length)], colliderData.gameObject.transform.position,
+                Random.Range(0, 360));
         }
     }
 }
