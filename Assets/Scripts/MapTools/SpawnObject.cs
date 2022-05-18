f900e4bd8c393c3250b9e62ee1cd86312a2d19dd
Markus Wyss <wyssmarkus@msn.com>
2022-05-18 16:06:30 +0200

    fix SpawnObject, set Parent Map

--- Assets/Scripts/MapTools/SpawnObject.cs
+++ Assets/Scripts/MapTools/SpawnObject.cs
@@ -6,8 +6,7 @@ public class SpawnObject : MonoBehaviour
 {
     public static void Spawn(GameObject obj, Vector3 vector, float angle)
     {
-        var spawned = Instantiate(obj, obj.transform);
-        
+        var spawned = Instantiate(obj, GameObject.Find("Map").transform);
         spawned.name = $"{obj.tag}X{vector.x}Y{vector.y}";
         spawned.transform.localPosition = new Vector3(vector.x, vector.y, vector.z);
         spawned.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
