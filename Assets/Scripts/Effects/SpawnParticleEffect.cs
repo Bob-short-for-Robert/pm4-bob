c575d5f598a918bd83d129fac6cbf0d3472b247b
Markus Wyss <wyssmarkus@msn.com>
2022-05-18 17:52:47 +0200

    add ParticleEffects

---
+++ Assets/Scripts/Effects/SpawnParticleEffect.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace Effects
+{
+    public class SpawnParticleEffect : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject spawnParticleEffect;
+        [SerializeField]
+        private bool onDestroy;
+        [SerializeField]
+        private bool onCollision;
+        [SerializeField]
+        private bool useTag;
+        [SerializeField]
+        private new string tag = string.Empty;
+        
+        
+        private void OnDestroy()
+        {
+            if (onDestroy)
+            { 
+                SpawnEffect();  
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (!onDestroy && onCollision && !useTag)
+            { 
+                SpawnEffect();  
+            }
+            if (!onDestroy && onCollision && useTag)
+            {
+                if (col.CompareTag(tag))
+                {
+                    SpawnEffect();   
+                }
+            }
+        }
+
+        private void SpawnEffect()
+        {
+            var effectObject = Instantiate(spawnParticleEffect, GameObject.Find("Map").transform);
+            var locationsVector = transform.localPosition;
+            effectObject.name = $"ParticleEffectX{locationsVector.x}Y{locationsVector.y}";
+            effectObject.transform.localPosition = locationsVector;
+            if (!spawnParticleEffect.CompareTag("Circle"))
+            { 
+                effectObject.transform.rotation = transform.rotation;  
+            }
+        }
+    }
+}
