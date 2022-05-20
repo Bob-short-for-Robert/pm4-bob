9d60a38668cd5bd34abcc2a615124569108991e3
DonTars <fischer.gabriel@outlook.de>
2022-05-20 18:56:05 +0200

    ice tower slows enemy

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,6 +4,8 @@ public class FollowPlayer : MonoBehaviour
 {
     public float speed = 2;
 
+    private float _slow = 0;
+    private float _isSlowedFor = 0;
     private GameObject _player;
 
     void Start()
@@ -13,7 +15,32 @@ public class FollowPlayer : MonoBehaviour
 
     void Update()
     {
+        if (_isSlowedFor <= 0)
+        {
+            _isSlowedFor = 0;
+            _slow = 0;
+        }
+        else
+        {
+            _isSlowedFor -= Time.deltaTime;
+        }
         transform.position =
-            Vector3.MoveTowards(transform.position, _player.transform.position, speed * Time.deltaTime);
+            Vector3.MoveTowards(transform.position, _player.transform.position, (speed - _slow) * Time.deltaTime);
     }
+    
+    public void ModifySpeed(float reduceSpeed, float timeSpan)
+    {
+        //avoid going over 0 speed by forcin
+        if (speed - reduceSpeed < 0)
+        {
+            _slow = speed;
+        }
+        else
+        {
+            _slow -= reduceSpeed;
+        }
+
+        _isSlowedFor += timeSpan;
+    }
+
 }
